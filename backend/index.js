9dc947a72ae4a92b1ec255f6d13057963e206957
al1211 <anil.kumar0372915@gmail.com>
2026-06-12 15:53:28 +0530

    fix cookies problem

--- backend/index.js
+++ backend/index.js
@@ -116,7 +116,7 @@ app.post("/api/auth/login", async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
-            sameSite: "lax",
+            sameSite: "none",
             maxAge: 24 * 60 * 60 * 1000
         })
 
