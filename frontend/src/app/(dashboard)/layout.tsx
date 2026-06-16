33595a65c89078a9d2d7a3b2be79012f0079d5cf
al1211 <anil.kumar0372915@gmail.com>
2026-06-16 12:03:13 +0530

    add interceptor in my frontend

--- frontend/src/app/(dashboard)/layout.tsx
+++ frontend/src/app/(dashboard)/layout.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { useAuthStore } from "../../../store/store";
 import Sidebar from "../../../components/layout/Sidebar";
+import "../../../lib/api"
 
 export default function DashboardLayout({
   children,
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,7 +2,6 @@
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useAuthStore } from "../../store/store";
-import { useEffect } from "react";
 
 export default function HomePage() {
 
